b9a87ce97cb4515cf0cd1ef97781f6a684669b05
Nícolas Gomes Pacheco <nicolasgomes.ngp@gmail.com>
2025-11-09 18:55:46 -0300

    2° Commit | 9/11/2025

---
+++ Projeto_ApiChatGptReceita/src/App.jsx
@@ -0,0 +1,15 @@
+import chatReceitas from "./pages/receitasChat"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+
+      <chatReceitas />
+
+    </div>
+  )
+
+}
+
+export default App
---
+++ Projeto_ApiChatGptReceita/src/pages/receitasChat.jsx
@@ -0,0 +1,22 @@
+
+
+const chatReceitas = () => {
+
+
+    return (
+        <div className="min-h-screen bg--gradient-to-br from-purple-500 via-gray-50 to-emerald-50 p-4">
+            <div className="container mx-auto max-w-4xl">
+                <header>
+                    <h1>Api Receitas</h1>
+                    <p>Seu assistente pessoal para receitas deliciosas!</p>
+                </header>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default chatReceitas
