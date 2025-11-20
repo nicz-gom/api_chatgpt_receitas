b83ca9d4f5aea402daf3706e4efcd5e300c3ea02
Nícolas Gomes Pacheco <nicolasgomes.ngp@gmail.com>
2025-11-20 10:31:01 -0300

    5° Commit | 20/11/2025 (Finalização)

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import Express from "express";
 import receitasRoutes from "./src/routes/receitas.route.js";
+import cors from 'cors'
 
 const app = Express();
 const PORT = 3001;
+
+app.use(cors())
 
 app.use(Express.json());
 
